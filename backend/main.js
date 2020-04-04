ce9b3949870ef8ea6ec96fb67d6607247f2eeb51
ColeyG <cjgeerts@gmail.com>
2020-04-04 03:04:45 -0400

    Adding Video Stamps

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,21 @@
 const five = require('johnny-five');
 const Picam = require('pi-camera');
 
+const timeStamp = (dateObject) => `${dateObject.getFullYear()}-${dateObject.getMonth() + 1}-${dateObject.getDate()}`;
+
+const videoStamp = (length) => {
+  const set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const date = new Date();
+  let resp = '';
+  for (let i = 0; i < length; i++) {
+    resp += set.charAt(Math.floor(Math.random() * set.length));
+  }
+  return `${timeStamp(date)}-${resp}`;
+};
+
 const camera = new Picam({
   mode: 'video',
-  output: `${__dirname}/clips/video.h264`,
+  output: `${__dirname}/clips/${videoStamp(5)}.h264`,
   width: 1920,
   height: 1080,
   timeout: 10000,
@@ -42,5 +54,9 @@ board.on('ready', () => {
 
   motion.on('motionend', () => {
     console.log('motionend');
+    if (!cameraOn) {
+      cameraOn = true;
+      clipRecord();
+    }
   });
 });
