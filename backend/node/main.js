6b628a35693a41abb6e6d92b93bbcd7968be3021
ColeyG <cjgeerts@gmail.com>
2020-04-04 01:53:13 -0400

    Clips

--- backend/node/main.js
+++ backend/node/main.js
@@ -2,6 +2,16 @@ const five = require('johnny-five');
 
 const board = new five.Board();
 let motion;
+let motionDetected;
+
+const clipRecord = () => {
+  console.log('Recording Clip');
+  setTimeout(() => {
+    if (motionDetected) {
+      clipRecord();
+    }
+  }, 5000);
+};
 
 board.on('ready', () => {
   motion = new five.Motion(7);
@@ -12,9 +22,11 @@ board.on('ready', () => {
 
   motion.on('motionstart', () => {
     console.log('motionstart');
+    motionDetected = true;
   });
 
   motion.on('motionend', () => {
     console.log('motionend');
+    motionDetected = false;
   });
 });
