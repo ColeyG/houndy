bf4592b67c1da7500a21a21012f85169c488aba7
ColeyG <cjgeerts@gmail.com>
2020-04-09 15:55:09 -0400

    Replicate Route Code

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,6 @@ const Log = require('../models/Log');
 
 const router = express.Router();
 
-
-// TODO: Routes for motion, gas, and light
 // TODO: Save Clips
 
 router.get('/light/:uid', (req, res, next) => {
@@ -37,6 +35,82 @@ router.get('/light/getLast/:uid', (req, res, next) => {
     });
   } else {
     Log.find({ logType: 'light' }).limit(parseInt(req.params.uid, 10)).then((logs) => {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success: "${JSON.stringify(logs)}}`);
+    });
+  }
+});
+
+router.get('/gas/:uid', (req, res, next) => {
+  const id = req.params.uid;
+
+  const log = new Log({ logType: 'gas', amount: id, time: new Date() });
+
+  log.save((err, resp) => {
+    if (err) {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end('{"error": "error saving data"}');
+    } else {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success": ${JSON.stringify(resp)}}`);
+    }
+  });
+});
+
+router.get('/gas/getLast/:uid', (req, res, next) => {
+  if (req.params.uid === '0') {
+    Log.find({ logType: 'gas' }).then((logs) => {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success: "${JSON.stringify(logs)}}`);
+    });
+  } else {
+    Log.find({ logType: 'gas' }).limit(parseInt(req.params.uid, 10)).then((logs) => {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success: "${JSON.stringify(logs)}}`);
+    });
+  }
+});
+
+router.get('/motion/:uid', (req, res, next) => {
+  const id = req.params.uid;
+
+  const log = new Log({ logType: 'light', amount: id, time: new Date() });
+
+  log.save((err, resp) => {
+    if (err) {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end('{"error": "error saving data"}');
+    } else {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success": ${JSON.stringify(resp)}}`);
+    }
+  });
+});
+
+router.get('/motion/getLast/:uid', (req, res, next) => {
+  if (req.params.uid === '0') {
+    Log.find({ logType: 'motion' }).then((logs) => {
+      res
+        .status(200)
+        .contentType('text/json')
+        .end(`{"success: "${JSON.stringify(logs)}}`);
+    });
+  } else {
+    Log.find({ logType: 'motion' }).limit(parseInt(req.params.uid, 10)).then((logs) => {
       res
         .status(200)
         .contentType('text/json')
