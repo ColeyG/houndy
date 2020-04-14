d31b9e8a9cb9c452adac0140f144340a4babbd20
ColeyG <cjgeerts@gmail.com>
2020-04-14 02:22:21 -0400

    Add Video PI server

---
+++ backend/serveVideos.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(express.static('clips'));
